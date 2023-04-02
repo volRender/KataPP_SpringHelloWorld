d3ee7bb66a503d83ab20f99f277c5be500f5bbb2
volRender <litvishko.render@gmail.com>
2023-04-02 13:27:52 +0300

    Task already done.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,20 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld beanHW1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld beanHW2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanHW1.getMessage());
+        System.out.println(beanHW2.getMessage());
+
+        //создание бина Cat
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat1.getMessage());
+        System.out.println(beanCat2.getMessage());
+
+        //сравнение бинов
+        System.out.println(beanHW1 == beanHW2);
+        System.out.println(beanCat1 == beanCat2);
     }
 }
